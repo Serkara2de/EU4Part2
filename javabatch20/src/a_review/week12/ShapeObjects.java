f00a6d538573a1fa2f86607933a83e5dbd3f7601
Serkara2de <53303465+Serkara2de@users.noreply.github.com>
2020-12-12 22:02:59 +0100

    Review week12

--- javabatch20/src/a_review/week12/ShapeObjects.java
+++ javabatch20/src/a_review/week12/ShapeObjects.java
@@ -3,5 +3,23 @@ package a_review.week12;
 public class ShapeObjects {
     public static void main(String[] args) {
 
+        //Rectangle : w=10 l=20
+
+        Rectangle rectangle = new Rectangle(10,20);
+        System.out.println(rectangle);
+
+
+        // Square side=4
+
+        Square square = new Square(4);
+        System.out.println(square);
+
+
+        // Circle r=8
+
+        Circle circle = new Circle(8);
+        System.out.println(circle);
+
+
     }
 }
