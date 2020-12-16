83827362f1d976a62dc56f7a40e537cbf5d78eed
Serkara2de <53303465+Serkara2de@users.noreply.github.com>
2020-12-16 01:27:20 +0100

    Lessons

---
+++ javabatch20/src/day40_accessmodifiers_final/CarTest.java
@@ -0,0 +1,19 @@
+package day40_accessmodifiers_final;
+
+public class CarTest {
+
+    public static void main(String[] args) {
+
+    Car car = new Car();
+
+    car.model= "BMW";
+    car.year = 2020;
+    car.engine= 2.5;
+    //car.door= 4;
+
+
+
+
+
+    }
+}
