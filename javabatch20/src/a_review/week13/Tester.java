e632e973ba170c9d9c8dec5d2ab032accf65c407
Serkara2de <53303465+Serkara2de@users.noreply.github.com>
2020-12-19 16:56:43 +0100

    Week 14 Review

--- javabatch20/src/a_review/week13/Tester.java
+++ javabatch20/src/a_review/week13/Tester.java
@@ -1,9 +1,18 @@
 package a_review.week13;
 
-public class Tester extends Employee {
+public final class Tester extends Employee { // Tester IS A Employee, Tester IS A Person
 
     public Tester(String name, int age, char gender, String ID, String jobTitle, double hourlyRate, boolean isFullTime) {
         super(name, age, gender, ID, jobTitle, hourlyRate, isFullTime);
+    }
+
+
+    public void findBug(){
+        System.out.println(jobTitle+" "+ " found a bug");
+    }
+
+    public void testing(){
+        System.out.println(jobTitle+" "+ " is testing the application");
     }
 
 
