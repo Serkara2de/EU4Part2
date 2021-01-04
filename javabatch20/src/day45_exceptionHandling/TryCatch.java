84fdc1a6069dee3e2e72412daf08f33623c9a451
Serkara2de <53303465+Serkara2de@users.noreply.github.com>
2021-01-05 00:01:41 +0100

    Day 45 Exceptions and Errors

---
+++ javabatch20/src/day45_exceptionHandling/TryCatch.java
@@ -0,0 +1,17 @@
+package day45_exceptionHandling;
+
+public class TryCatch {
+    public static void main(String[] args) {
+
+
+        try {
+            String str = "java";
+            System.out.println(str.charAt(0));
+            System.out.println(str.charAt(5)); // i might have problem here
+            System.out.println(str.charAt(1)); // WILL NOT EXECUTED
+        }catch(Exception e){
+            System.out.println("Exception happened in try block and was caught");
+        }
+        System.out.println("After try catch block");
+    }
+}
